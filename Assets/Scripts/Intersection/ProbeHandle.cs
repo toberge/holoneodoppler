f0d567fa77029ce8fb2f92fa087e4a49983079f6
Tore Bergebakken <toberge@stud.ntnu.no>
2023-03-03 17:00:55 +0100

    Make probe model snap to skull

--- Assets/Scripts/Intersection/ProbeHandle.cs
+++ Assets/Scripts/Intersection/ProbeHandle.cs
@@ -1,14 +1,16 @@
-using System;
 using Microsoft.MixedReality.Toolkit.Input;
 using UnityEngine;
 
 public class ProbeHandle : MonoBehaviour, IMixedRealityPointerHandler
 {
     [SerializeField] private Transform skull;
+    [SerializeField] private Transform probe;
     private Transform originalParent;
+    private LayerMask skullMask;
 
     private void Start()
     {
+        skullMask = LayerMask.GetMask("SkullSnap");
         originalParent = transform.parent;
     }
 
@@ -31,10 +33,20 @@ public class ProbeHandle : MonoBehaviour, IMixedRealityPointerHandler
 
     private void FixedUpdate()
     {
-        // Always look to the center of the skull!
-        transform.rotation = Quaternion.LookRotation(transform.position - skull.position);
+        var direction = (skull.position - transform.position).normalized;
+        // Probe should snap to skull
+        var didHit = Physics.Raycast(transform.position, direction, out RaycastHit hit, 10000f, skullMask);
+        Debug.Assert(didHit, $"Somehow did not hit skull from {transform.position} to {skull.position}");
+        probe.position = hit.point - direction * 0.005f;
+        // Probe should always look to the center of the skull!
+        probe.rotation = Quaternion.LookRotation(-direction);
     }
 
-    public void OnPointerClicked(MixedRealityPointerEventData eventData) {}
-    public void OnPointerDragged(MixedRealityPointerEventData eventData) {}
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+    }
 }
