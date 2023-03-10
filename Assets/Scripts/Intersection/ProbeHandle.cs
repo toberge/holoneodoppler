8bf52bded5c63f8f71ce46a6ea288db80099549c
Tore Bergebakken <toberge@stud.ntnu.no>
2023-03-10 18:54:26 +0100

    Make users able to freely rotate probe
    - Implemented as default rotation mode
    - Tweaked behaviour of probe handle to account for potential weirdness after rotating

--- Assets/Scripts/Intersection/ProbeHandle.cs
+++ Assets/Scripts/Intersection/ProbeHandle.cs
@@ -7,6 +7,8 @@ public class ProbeHandle : MonoBehaviour, IMixedRealityPointerHandler
     [SerializeField] private Transform probe;
     private Transform originalParent;
     private LayerMask skullMask;
+    private bool grabbed = false;
+    private bool useFixedRotation = false;
 
     public delegate void GrabEvent();
 
@@ -16,35 +18,56 @@ public class ProbeHandle : MonoBehaviour, IMixedRealityPointerHandler
     {
         skullMask = LayerMask.GetMask("SkullSnap");
         originalParent = transform.parent;
+        probe.rotation = Quaternion.LookRotation(-DirectionToCenter());
+    }
+
+    public void ToggleFixedRotation()
+    {
+        useFixedRotation = !useFixedRotation;
+    }
+
+    private Vector3 DirectionToCenter()
+    {
+        return (centerOfSkull.position - transform.position).normalized;
     }
 
     public void OnPointerDown(MixedRealityPointerEventData eventData)
     {
-        if (eventData.Pointer is SpherePointer pointer)
-        {
-            OnGrab?.Invoke();
-            transform.parent = pointer.transform;
-        }
+        if (!(eventData.Pointer is SpherePointer pointer)) return;
+        OnGrab?.Invoke();
+        grabbed = true;
+        // Prepare for moving the probe
+        transform.rotation = probe.rotation;
+        // Reparent to pointer
+        transform.parent = pointer.transform;
     }
 
     public void OnPointerUp(MixedRealityPointerEventData eventData)
     {
-        if (eventData.Pointer is SpherePointer)
-        {
-            // Set parent without affecting world position
-            transform.SetParent(originalParent, true);
-        }
+        if (!(eventData.Pointer is SpherePointer)) return;
+        grabbed = false;
+        // Set parent without affecting world position
+        transform.SetParent(originalParent, true);
+        // Then reset to probe's position
+        transform.position = probe.position - .02f * DirectionToCenter();
     }
 
     private void FixedUpdate()
     {
-        var direction = (centerOfSkull.position - transform.position).normalized;
+        var direction = DirectionToCenter();
         // Probe should snap to skull
-        var didHit = Physics.Raycast(transform.position, direction, out RaycastHit hit, 10000f, skullMask);
+        var didHit = Physics.Raycast(transform.position, direction, out var hit, 10000f, skullMask);
         Debug.Assert(didHit, $"Somehow did not hit skull from {transform.position} to {centerOfSkull.position}");
         probe.position = hit.point - direction * 0.005f;
-        // Probe should always look to the center of the skull!
-        probe.rotation = Quaternion.LookRotation(-direction);
+        if (useFixedRotation)
+        {
+            // Probe should always look to the center of the skull!
+            probe.rotation = Quaternion.LookRotation(-direction);
+        }
+        else if (grabbed)
+        {
+            probe.rotation = transform.rotation;
+        }
     }
 
     public void OnPointerClicked(MixedRealityPointerEventData eventData)
